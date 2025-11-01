b477e5ca96a5c2831dc0ab7db165d1a1c641cfec
Juan Miguel Recondo <134484809+jmrec@users.noreply.github.com>
2025-11-01 08:45:28 +0800

    temp: comment out book and user API implementations

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -19,12 +19,12 @@ import (
 	"golang.org/x/time/rate"
 )
 
-func ContextMiddleware(bookRepository BookRepository) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Set("appCtx", bookRepository)
-		c.Next()
-	}
-}
+//func ContextMiddleware(bookRepository BookRepository) gin.HandlerFunc {
+//	return func(c *gin.Context) {
+//		c.Set("appCtx", bookRepository)
+//		c.Next()
+//	}
+//}
 
 func NewRouter(
 	logger *zap.Logger,
@@ -33,11 +33,11 @@ func NewRouter(
 	redisClient cache.Cache,
 	ctx *context.Context,
 ) *gin.Engine {
-	bookRepository := NewBookRepository(db, redisClient, ctx)
-	userRepository := NewUserRepository(db, ctx)
+	//bookRepository := NewBookRepository(db, redisClient, ctx)
+	//userRepository := NewUserRepository(db, ctx)
 
 	r := gin.Default()
-	r.Use(ContextMiddleware(bookRepository))
+	//r.Use(ContextMiddleware(bookRepository))
 
 	//r.Use(gin.Logger())
 	r.Use(middleware.Logger(logger, mongoCollection))
@@ -49,17 +49,17 @@ func NewRouter(
 	r.Use(middleware.RateLimiter(rate.Every(1*time.Minute), 60)) // 60 requests per minute
 
 	docs.SwaggerInfo.BasePath = "/api/v1"
-	v1 := r.Group("/api/v1")
+	//v1 := r.Group("/api/v1")
 	{
-		v1.GET("/", bookRepository.Healthcheck)
-		v1.GET("/books", middleware.APIKeyAuth(), bookRepository.FindBooks)
-		v1.POST("/books", middleware.APIKeyAuth(), middleware.JWTAuth(), bookRepository.CreateBook)
-		v1.GET("/books/:id", middleware.APIKeyAuth(), bookRepository.FindBook)
-		v1.PUT("/books/:id", middleware.APIKeyAuth(), bookRepository.UpdateBook)
-		v1.DELETE("/books/:id", middleware.APIKeyAuth(), bookRepository.DeleteBook)
+		//v1.GET("/", bookRepository.Healthcheck)
+		//v1.GET("/books", middleware.APIKeyAuth(), bookRepository.FindBooks)
+		//v1.POST("/books", middleware.APIKeyAuth(), middleware.JWTAuth(), bookRepository.CreateBook)
+		//v1.GET("/books/:id", middleware.APIKeyAuth(), bookRepository.FindBook)
+		//v1.PUT("/books/:id", middleware.APIKeyAuth(), bookRepository.UpdateBook)
+		//v1.DELETE("/books/:id", middleware.APIKeyAuth(), bookRepository.DeleteBook)
 
-		v1.POST("/login", middleware.APIKeyAuth(), userRepository.LoginHandler)
-		v1.POST("/register", middleware.APIKeyAuth(), userRepository.RegisterHandler)
+		//v1.POST("/login", middleware.APIKeyAuth(), userRepository.LoginHandler)
+		//v1.POST("/register", middleware.APIKeyAuth(), userRepository.RegisterHandler)
 	}
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
--- pkg/database/postgres/db_test.go
+++ pkg/database/postgres/db_test.go
@@ -1,16 +1,17 @@
 package postgres
 
-import (
-	"testing"
-
-	"github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/assert"
-)
-
-func TestNewDatabase(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	db := NewMockDatabase(ctrl)
-	assert.NotNil(t, db)
-}
+//
+//import (
+//	"testing"
+//
+//	"github.com/golang/mock/gomock"
+//	"github.com/stretchr/testify/assert"
+//)
+//
+//func TestNewDatabase(t *testing.T) {
+//	ctrl := gomock.NewController(t)
+//	defer ctrl.Finish()
+//
+//	db := NewMockDatabase(ctrl)
+//	assert.NotNil(t, db)
+//}
